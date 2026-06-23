7ba74acec94aa7b6c055546d2d0ad1c0a07aee8e
vinzanaw <nafinzaptri@gmail.com>
2026-06-23 18:59:30 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,50 @@
+tugas_list = []
+def todo_menu():
+    print("\n====== TO DO LIST =====")
+    print("1. Tambah Tugas")
+    print("2. Lihat Tugas")
+    print("3. Hapus Tugas")
+    print("4. Keluar")
+    print("=======================")
+
+def tambah_tugas():
+    tugas = input("Masukkan Tugas : ")
+    tugas_list.append(tugas)
+    print("Tugas Berhasil Ditambah, Ayo Kita Lihat Tugasnya")
+
+def lihat_tugas():
+    if len(tugas_list) == 0:
+        print("❌ TIdak Ada Yang Bisa Dilihat, Ayo Tambahkan Terlebih Dahulu")
+    else: 
+        for i in range(len(tugas_list)):
+            print(f"{i+1}. {tugas_list[i]}") 
+    
+def hapus_tugas():
+   if len(tugas_list) == 1: 
+        lihat_tugas()
+        hapus = int(input("Yang Mana Yang Mau Kamu Hapus Nih? : "))
+        tugas_list.pop(hapus - 1)
+        print("Tugas Berhasil Dihapus")
+
+   else:
+        print("❌ Tidak Ada Yang Bisa Dihapus, Ayo Tambahkan Terlebih Dahulu")
+
+
+while True:
+    todo_menu()
+    menu = input("Masukkan Pilihan : ")
+    if menu == "1":
+        tambah_tugas()
+    elif menu == "2":
+        lihat_tugas()
+    elif menu == "3":
+        hapus_tugas()
+    elif menu == "4":
+        print("Terimakasih Sudah Menggunakan TODO LIST INI 🤍 !! ")
+        break
+    elif menu == "":
+        print("Pilihan Tidak Boleh Kosong")
+    else: 
+        print("Tidak Ada Menu itu Silahkan Pilih Yang Ada Diatas 💢 !!")
+
+
